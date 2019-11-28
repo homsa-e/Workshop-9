740e738e038f1f56a320ee37086d481e28cc2cb4
George <piatskgeorge@mail.ru>
2019-11-28 11:17:24 +0300

    change vector of vectors to map of structs

--- ex1/main.cpp
+++ ex1/main.cpp
@@ -4,6 +4,9 @@
 #include <vector>
 #include <map>
 #include <unordered_set>
+#include <cassert>
+#include <algorithm>
+
 
 enum worldcities_data_columns
 {
@@ -21,12 +24,49 @@ enum worldcities_data_columns
     total_columns
 };
 
-std::vector<std::vector<std::string>> worldCities ()
+std::string no_quotes (const std::string &input)
+{
+    std::string result = input;
+    result.erase(std::remove(result.begin(), result.end(), '"'), result.end());
+    return result;
+}
+
+struct city_data
+{
+    city_data ()
+    {}
+
+    city_data (const std::vector <std::string> &tokens) :
+        lat (std::stod (no_quotes (tokens[worldcities_data_columns::lat]))),
+        lng (std::stod (no_quotes (tokens[worldcities_data_columns::lng]))),
+        country (no_quotes (tokens[worldcities_data_columns::country])),
+        is_capital (no_quotes (tokens[worldcities_data_columns::capital]) == "primary")
+    {
+        std::string population_string = no_quotes (tokens[worldcities_data_columns::population]);
+        if (population_string.empty ())
+        {
+            has_population = false;
+        }
+        else
+        {
+            has_population = true;
+            population = std::stod (population_string);
+        }
+    }
+    double lat;
+    double lng;
+    double population;
+    bool has_population;
+    std::string country;
+    bool is_capital;
+};
+
+std::map<std::string, city_data> worldCities ()
 {
     std::ifstream in ("worldcities.csv");
     std::string line;
 
-    std::vector<std::vector<std::string>> result;
+    std::map<std::string, city_data> result;
 
     std::getline (in, line);
     while (!in.eof() && in.good ())
@@ -39,21 +79,42 @@ std::vector<std::vector<std::string>> worldCities ()
       while (!ss.eof () && ss.good ())
       {
           std::getline (ss, token, ',');
-          tokens.push_back (token);
+
+          if (!tokens.empty () &&
+              !tokens[tokens.size () - 1].empty () &&
+
+               tokens[tokens.size () - 1].front () == '"'
+               &&
+               (tokens[tokens.size () - 1].back () != '"'
+               ||
+               tokens[tokens.size () - 1].size () == 1))
+          {
+              tokens[tokens.size () - 1] += "," + token;
+          }
+          else
+          {
+              tokens.push_back (token);
+          }
       }
 
-      if (tokens.size () != total_columns)
-        result.push_back (tokens);
+
+      std::cout << result.size() << std::endl;
+      if (tokens.size () == total_columns)
+        result[tokens[worldcities_data_columns::city_ascii]] = city_data (tokens);
+      else
+      {
+        assert (ss.eof () || !ss.good ());
+      }
     }
     return result;
 }
 
-size_t howManyCountries (const std::vector<std::vector<std::string>> &world_cities)
+size_t howManyCountries (const std::map<std::string, city_data> &world_cities)
 {
     std::unordered_set<std::string> countries;
-    for (const std::vector<std::string> &line : world_cities)
+    for (const std::pair<const std::string, city_data> &data : world_cities)
     {
-        countries.insert (line[country]);
+        countries.insert (data.second.country);
     }
     return countries.size ();
 }
@@ -61,7 +122,7 @@ size_t howManyCountries (const std::vector<std::vector<std::string>> &world_citi
 
 int main ()
 {
-  std::vector<std::vector<std::string>> world_cities = worldCities ();
+  std::map<std::string, city_data> world_cities = worldCities ();
   size_t number_of_countries = howManyCountries (world_cities);
   std::cout << number_of_countries << std::endl;
   return 0;
